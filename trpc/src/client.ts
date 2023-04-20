9baa9165d97a279d44ae65335d0ed32dd11788a8
AlfieriChou <alfierichou@gmail.com>
2023-04-20 18:01:56 +0800

    chore: lint

--- trpc/src/client.ts
+++ trpc/src/client.ts
@@ -2,13 +2,13 @@ import {
   createTRPCProxyClient,
   httpBatchLink,
   loggerLink,
-} from 'npm:@trpc/client';
-import type { AppRouter } from './router.ts';
+} from "npm:@trpc/client";
+import type { AppRouter } from "./router.ts";
 
 const sleep = (ms = 100) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function main() {
-  const url = 'http://127.0.0.1:8000/trpc';
+  const url = "http://127.0.0.1:8000/trpc";
 
   const proxy = createTRPCProxyClient<AppRouter>({
     links: [loggerLink(), httpBatchLink({ url })],
@@ -20,21 +20,21 @@ async function main() {
   await Promise.all([
     //
     proxy.hello.query(),
-    proxy.hello.query('client'),
+    proxy.hello.query("client"),
   ]);
   await sleep();
 
   const postCreate = await proxy.post.createPost.mutate({
-    title: 'hello client',
+    title: "hello client",
   });
-  console.log('created post', postCreate.title);
+  console.log("created post", postCreate.title);
   await sleep();
 
   const postList = await proxy.post.listPosts.query();
-  console.log('has posts', postList, 'first:', postList[0].title);
+  console.log("has posts", postList, "first:", postList[0].title);
   await sleep();
 
-  console.log('👌 should be a clean exit if everything is working right');
+  console.log("👌 should be a clean exit if everything is working right");
 }
 
 main();
--- yaml/main.ts
+++ yaml/main.ts
@@ -3,9 +3,11 @@ import {
   stringify as yamlStringify,
 } from "https://deno.land/std@0.184.0/yaml/mod.ts";
 
-const yamlContent = await Deno.readTextFile('test.yaml');
+const yamlContent = await Deno.readTextFile("test.yaml");
 
 const data = await yamlParser(yamlContent) as Record<string, any>;
-console.log('json', data);
+console.log("json", data);
 
-console.log('yaml', yamlStringify(data));
+console.log("yaml", yamlStringify(data));
+
+console.log("yaml", yamlStringify(data));
