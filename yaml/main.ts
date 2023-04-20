ebdee2aa70e1f811bdbd86132a05815d3c0f09b5
AlfieriChou <alfierichou@gmail.com>
2023-04-20 17:27:30 +0800

    feat: read yaml file

---
+++ yaml/main.ts
@@ -0,0 +1,11 @@
+import {
+  parse as yamlParser,
+  stringify as yamlStringify,
+} from "https://deno.land/std@0.184.0/yaml/mod.ts";
+
+const yamlContent = await Deno.readTextFile('test.yaml');
+
+const data = await yamlParser(yamlContent) as Record<string, any>;
+console.log('json', data);
+
+console.log('yaml', yamlStringify(data));
